5de0494d303ab4165b74778836966dd013cf873a
Turi04470 <77255924+Turi04470@users.noreply.github.com>
2022-12-27 23:18:58 -0600

    cambio menores

--- Proyecto diseño/Controlador/Registrar.ashx.cs
+++ Proyecto diseño/Controlador/Registrar.ashx.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.UI.WebControls;
+
 
 namespace Proyecto_diseño.Controlador
 {
@@ -13,8 +17,42 @@ namespace Proyecto_diseño.Controlador
 
         public void ProcessRequest(HttpContext context)
         {
+            
+            /*
             context.Response.ContentType = "text/plain";
             context.Response.Write("Hola a todos");
+            context.Response.Write(Environment.MachineName);
+            */
+
+
+            string correo_login = context.Request["correo_login"];
+            string pass_login = context.Request["correo_login"];
+
+
+            string sql = "Execute registar_user N'Turi' , N'Turi@gmail.com' ,N'Turi', 0;";
+
+
+            Conexion conexion = new Conexion();
+
+            try
+            {
+                SqlCommand cmd = new SqlCommand(sql, conexion.Open());
+                int n = cmd.ExecuteNonQuery();
+
+            }
+            catch (Exception)
+            {
+               
+                throw;
+            }
+
+            conexion.Close();
+
+
+
+
+
+
         }
 
         public bool IsReusable
