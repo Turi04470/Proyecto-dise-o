30eec7484ea45fc07a38f0c3e1078f39d64b5747
Turi04470 <77255924+Turi04470@users.noreply.github.com>
2022-12-27 23:12:29 -0600

    Conexion correcta
    metodos para abrir y cerrar una conexion

--- Proyecto diseño/Conexion.cs
+++ Proyecto diseño/Conexion.cs
@@ -1,36 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI.WebControls.WebParts;
-
 using System.Data;
 using System.Data.Sql;
-using System.Data.SqlClient;
 
-
-namespace Pruebas
+namespace Proyecto_diseño
 {
     public class Conexion
     {
-        public SqlConnection GetConexion()
+        public SqlConnection Open()
         {
-            
-
-            try
-			{
-                string cadena = "Data Source=HERAT__;Initial Catalog=FACTURA_BASE;Integrated Security=True";
-                SqlConnection con = new SqlConnection(cadena);
-                con.Open();
-                return con;
-
-
-            }
-			catch (Exception)
-			{
-
-                return null;
-			}
+            string cadena = "Data Source=HERAT__;Initial Catalog=VIDEOJUEGOS;Integrated Security=True";
+            SqlConnection con = new SqlConnection(cadena);
+            con.Open();
+            return con;
         }
+
+
+        public SqlConnection Close()
+        {
+            string cadena = "Data Source=HERAT__;Initial Catalog=VIDEOJUEGOS;Integrated Security=True";
+            SqlConnection con = new SqlConnection(cadena);
+            con.Close();
+            return con;
+        }
+
     }
 }
