10428659816cae7f9ad3a635443f425fe739857c
Turi04470 <darkshadow04480@gmail.com>
2023-05-12 21:39:32 -0600

    Revert "Revert "Primer carga""
    This reverts commit cf0715da25880d28e4ea39ae48a6fe3a180a7ada.

--- Proyecto diseño/WEB/Inicio.aspx.designer.cs
+++ Proyecto diseño/WEB/Inicio.aspx.designer.cs
@@ -13,59 +13,5 @@ namespace Proyecto_diseño.WEB
 
     public partial class Inicio
     {
-
-        /// <summary>
-        /// Control year.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.DropDownList year;
-
-        /// <summary>
-        /// Control SqlData_Year.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.SqlDataSource SqlData_Year;
-
-        /// <summary>
-        /// Control categoria.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.DropDownList categoria;
-
-        /// <summary>
-        /// Control SqlData_categoria.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.SqlDataSource SqlData_categoria;
-
-        /// <summary>
-        /// Control plataforma.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.DropDownList plataforma;
-
-        /// <summary>
-        /// Control SqlData_plataforma.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.SqlDataSource SqlData_plataforma;
     }
 }
